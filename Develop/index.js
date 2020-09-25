0194252a7d10af0340b0d2ff11266ee4312eb49f
Heather Everton <heather.everton@planplusonline.com>
2020-09-25 15:17:33 -0600

    generate webpage regactor

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const [projectName] = readmeDataArgs;
  
 // const inquirer = require('inquirer');
 const fs = require('fs');
-// const generatePage = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // // array of questions for user
 // const questions = [
@@ -32,26 +32,8 @@ const fs = require('fs');
 
 // const generatePage = (projectName) => `ProjectName: ${projectName}`;
 // console.log(generatePage(projectName));
-
-const generatePage = (projectName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>${projectName}</title>
-    </head>
   
-    <body>
-      <h1>${projectName}</h1>
-    </body>
-    </html>
-    `;
-  };
-  
-  fs.writeFile('README.md', generatePage(projectName), err => {
+  fs.writeFile('README.md', generateMarkdown(projectName), err => {
     if (err) throw err;
   
     console.log('Readme complete! Check out README.md to see the output!');
